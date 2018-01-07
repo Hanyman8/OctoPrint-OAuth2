c924c6497cd6d8051d76420f02bc4c89448f37d5
Jiri Hanus notebook <hanusji8@fit.cvut.cz>
2018-01-07 02:22:01 +0100

    Finally works downgrade plugin

--- octoprint_oauthfit/__init__.py
+++ octoprint_oauthfit/__init__.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-from octoprint_oauthfit.oauth_user_manager import FilebasedUserManager
+from octoprint_oauthfit.oauth_user_manager import OAuthbasedUserManager
 import octoprint.plugin
 import logging
 
@@ -11,17 +11,16 @@ class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
 					   octoprint.plugin.OctoPrintPlugin):
 	def on_after_startup(self):
 		self._logger.info("############# Hello World FIT OAuth 0003!")
-		hooks = self._plugin_manager.get_hooks("octoprint.plugin.octoprint_oauthfit")
-		for name, hook in hooks.items():
-			hook()
+		#hooks = self._plugin_manager.get_hooks("octoprint.plugin.octoprint_oauthfit")
+		#for name, hook in hooks.items():
+		#	hook()
 
 	#def on_after_startup(self):
 	#	self._logger.info("############### Order Test Plugin: StartupPlugin.on_after_startup called")
 
 def user_factory_hook(components, settings, *args, **kwargs):
-	logging.getLogger(__name__).info("#######111111######")
-	print("11111111111")
-	return FilebasedUserManager()
+	logging.getLogger("octoprint.plugins." + __name__).info("#######111111######")
+	return OAuthbasedUserManager()
 
 __plugin_name__ = "OAuth"
 __plugin_implementation__ = HelloWorldPlugin()
--- octoprint_oauthfit/oauth_user_manager.py
+++ octoprint_oauthfit/oauth_user_manager.py
@@ -7,7 +7,9 @@ import os
 import yaml
 import uuid
 
+import wrapt
 
+from werkzeug.local import LocalProxy
 from octoprint.users import UserManager
 from octoprint.settings import settings
 from octoprint.users import User
@@ -18,18 +20,20 @@ from octoprint.util import atomic_write, to_str, deprecated
 
 
 #def user_factory_hook(components, settings, *args, **kwargs):
-#	return FilebasedUserManager
+#	return OAuthbasedUserManager
 
-class FilebasedUserManager(UserManager):
+class OAuthbasedUserManager(UserManager):
 	def __init__(self):
 
-		print("222222")
-		logging.getLogger(__name__).info("#######2222######")
+		logging.getLogger("octoprint.plugins." + __name__).info("#######2222######")
 		UserManager.__init__(self)
 
 		userfile = settings().get(["accessControl", "userfile"])
+		#logging.getLogger("octoprint.plugins." + __name__).info(userfile.items() + "  <<<< Userfile")
 		if userfile is None:
-			userfile = os.path.join(settings().getBaseFolder("base"), "users.yaml")
+			logging.getLogger("octoprint.plugins." + __name__).info("#######3333 - no userfile ######")
+			userfile = os.path.join(settings().getBaseFolder("base"), "users2.yaml")
+			print(userfile)
 		self._userfile = userfile
 		self._users = {}
 		self._dirty = False
@@ -39,11 +43,13 @@ class FilebasedUserManager(UserManager):
 
 	def _load(self):
 		if os.path.exists(self._userfile) and os.path.isfile(self._userfile):
+			logging.getLogger("octoprint.plugins." + __name__).info("#######4444 - path to new userfile exists ######")
 			self._customized = True
 			with open(self._userfile, "r") as f:
 				data = yaml.safe_load(f)
 				for name in data.keys():
 					attributes = data[name]
+					print(data.items())
 					apikey = None
 					if "apikey" in attributes:
 						apikey = attributes["apikey"]
@@ -55,6 +61,7 @@ class FilebasedUserManager(UserManager):
 						if sessionid in self._session_users_by_session:
 							self._session_users_by_session[sessionid].update_user(self._users[name])
 		else:
+			logging.getLogger("octoprint.plugins." + __name__).info("#######4444 - NOT Exitsting user file ######")
 			self._customized = False
 
 	def _save(self, force=False):
@@ -77,6 +84,36 @@ class FilebasedUserManager(UserManager):
 			self._dirty = False
 		self._load()
 
+	def login_user(self, user):
+		self._cleanup_sessions()
+
+		logging.getLogger("octoprint.plugins." + __name__).info("#######5555 - My login ######")
+		print(" MY LOGIN ")
+		if user is None:
+			return
+
+		if isinstance(user, LocalProxy):
+			user = user._get_current_object()
+
+		if not isinstance(user, User):
+			return None
+
+		if not isinstance(user, SessionUser):
+			user = SessionUser(user)
+
+		self._session_users_by_session[user.session] = user
+
+		userid = user.get_id()
+		if not userid in self._sessionids_by_userid:
+			self._sessionids_by_userid[userid] = set()
+
+		self._sessionids_by_userid[userid].add(user.session)
+		logging.getLogger("octoprint.plugins." + __name__).info("#######6666 - My login ######")
+
+		self._logger.debug("Logged in user: %r" % user)
+
+		return user
+
 	def addUser(self, username, password, active=False, roles=None, apikey=None, overwrite=False):
 		if not roles:
 			roles = ["user"]
@@ -84,7 +121,7 @@ class FilebasedUserManager(UserManager):
 		if username in self._users.keys() and not overwrite:
 			raise UserAlreadyExists(username)
 
-		self._users[username] = User(username, UserManager.createPasswordHash(password), active, roles, apikey=apikey)
+		self._users[username] = User(username, password, active, roles, apikey=apikey)
 		self._dirty = True
 		self._save()
 
@@ -207,10 +244,32 @@ class FilebasedUserManager(UserManager):
 		self._dirty = True
 		self._save()
 
+	def findUserFromBase(self, userid=None, session=None):
+		if session is not None:
+			print("OK - session not none")
+		else:
+			print("BAD - session IS none")
+
+		if session in self._session_users_by_session:
+			print("OK - session in users by session")
+		else:
+			print("BAD - session not in users")
+
+		if session is not None and session in self._session_users_by_session:
+			user = self._session_users_by_session[session]
+			if userid is None or userid == user.get_id():
+				return user
+
+		return None
+
 	def findUser(self, userid=None, apikey=None, session=None):
-		user = UserManager.findUser(self, userid=userid, session=session)
+
+		user = self.findUserFromBase(userid=userid, session=session)
+
+		print("trying t find user")
 
 		if user is not None:
+			print("  *** USER FOUND ***" + user.get_id())
 			return user
 
 		if userid is not None:
@@ -227,8 +286,73 @@ class FilebasedUserManager(UserManager):
 		else:
 			return None
 
+	def checkPassword(self, username, password):
+		print("Checking password")
+		user = self.findUser(username)
+		if not user:
+			print("Not user")
+			return False
+
+		if user.check_password(password):
+			# new hash matches, correct password
+			return True
+		else:
+			return False
+
 	def getAllUsers(self):
 		return map(lambda x: x.asDict(), self._users.values())
 
 	def hasBeenCustomized(self):
 		return self._customized
+
+class SessionUser(wrapt.ObjectProxy):
+	def __init__(self, user):
+		print(" MAKING session user")
+		wrapt.ObjectProxy.__init__(self, user)
+
+		import string
+		import random
+		import time
+		chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
+		self._self_session = "".join(random.choice(chars) for _ in range(10))
+		self._self_created = time.time()
+
+	@property
+	def session(self):
+		return self._self_session
+
+	@property
+	def created(self):
+		return self._self_created
+
+	@deprecated("SessionUser.get_session() has been deprecated, use SessionUser.session instead", since="1.3.5")
+	def get_session(self):
+		return self.session
+
+	def update_user(self, user):
+		self.__wrapped__ = user
+
+	def __repr__(self):
+		return "SessionUser({!r},session={},created={})".format(self.__wrapped__, self.session, self.created)
+
+class DummyUser(User):
+	def __init__(self):
+		User.__init__(self, "dummy", "", True, UserManager.valid_roles)
+
+	def check_password(self, passwordHash):
+		return True
+
+class DummyIdentity(Identity):
+	def __init__(self):
+		Identity.__init__(self, "dummy")
+
+def dummy_identity_loader():
+	return DummyIdentity()
+
+
+##~~ Apiuser object to use when global api key is used to access the API
+
+
+class ApiUser(User):
+	def __init__(self):
+		User.__init__(self, "_api", "", True, UserManager.valid_roles)
