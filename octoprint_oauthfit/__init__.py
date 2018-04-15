e9b14d776c752118e1ed4fb2c1f466fb780f2132
Jiri Hanus notebook <hanusji8@fit.cvut.cz>
2018-04-15 19:59:11 +0200

    Replace initial login form

--- octoprint_oauthfit/__init__.py
+++ octoprint_oauthfit/__init__.py
@@ -34,10 +34,10 @@ class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
 	def get_template_configs(self):
 		self._logger.info("************ Template configs *************")
 		return [
-			dict(type="navbar", template="oauthfit_login.jinja2", custom_bindings=False),
-			dict(type="navbar", template="oauthfit_name.jinja2"),
-			dict(type="navbar", custom_bindings=False),
-			dict(type="settings", custom_bindings=False)
+			dict(type="navbar", template="oauthfit_login.jinja2", custom_bindings=False, replaces="login"),
+			# dict(type="navbar", template="oauthfit_name.jinja2", custom_bindings=False),
+			# dict(type="navbar", custom_bindings=True),
+			dict(type="settings", custom_bindings=True)
 		]
 
 
--- octoprint_oauthfit/static/js/oauthfit.js
+++ octoprint_oauthfit/static/js/oauthfit.js
@@ -1,8 +1,28 @@
 $(function() {
-    function OAuthViewModel(parameters) {
+    function OAuthLoginModel(parameters) {
         var self = this;
 
-        self.settings = parameters[0];
+
+
+        $(".dropdown-menu input").click(function (e) {
+            e.stopPropagation();
+        });
+
+        self.loginState = parameters[0];
+        self.settings = parameters[1];
+        self.control = parameters[2];
+
+        // self.loginState.loginUser = ko.observable(false);
+        // self.loginState = ko.observable(false);
+
+        self.loginState.userMenuText = ko.pureComputed(function () {
+           if (self.loginState.loggedIn()){
+               return self.loginState.username();
+           }
+           else {
+               return gettext("Login_from_model");
+           }
+        });
 
         // this will hold the URL currently displayed by the iframe
         self.currentUrl = ko.observable();
@@ -10,14 +30,15 @@ $(function() {
         // this will hold the URL entered in the text field
         self.newUrl = ko.observable();
 
-      //  self.myMessage = ko.observable();
-        //self.myMessage("Ahoj");
+
 
         // this will be called when the user clicks the "Go" button and set the iframe's URL to
         // the entered URL
         self.goToUrl = function() {
             self.currentUrl(self.newUrl());
         };
+
+
 
         // This will get called before the HelloWorldViewModel gets bound to the DOM, but after its
         // dependencies have already been initialized. It is especially guaranteed that this method
@@ -27,21 +48,24 @@ $(function() {
             self.newUrl(self.settings.settings.plugins.oauthfit.url());
             self.goToUrl();
         }
+
+
+
     }
   //  OAuthViewModel.myMessage("Nazdar");
 
     // This is how our plugin registers itself with the application, by adding some configuration
     // information to the global variable OCTOPRINT_VIEWMODELS
-    OCTOPRINT_VIEWMODELS.push([
+    OCTOPRINT_VIEWMODELS.push({
         // This is the constructor to call for instantiating the plugin
-        OAuthViewModel,
+        construct: OAuthLoginModel,
 
         // This is a list of dependencies to inject into the plugin, the order which you request
         // here is the order in which the dependencies will be injected into your view model upon
         // instantiation via the parameters argument
-        ["settingsViewModel"],
+        dependencies: ["loginStateViewModel", "settingsViewModel", "controlViewModel"],
 
         // Finally, this is the list of selectors for all elements we want this view model to be bound to.
-        ["#tab_plugin_oauthfit"]
-    ]);
+        elements: ["#tab_plugin_oauthfit"]
+    });
 });
