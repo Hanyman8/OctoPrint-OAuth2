f7ff879930ee876b92df434c670ed0542f9dfea3
Jiří Hanuš <hanusji8@fit.cvut.cz>
2018-05-05 22:41:08 +0200

    Use oauthlib library to make OAuth2 authorization

--- octoprint_oauth2/oauth_user_manager.py
+++ octoprint_oauth2/oauth_user_manager.py
@@ -1,152 +1,143 @@
 from octoprint.users import *
-import requests
-import requests.auth
+from requests_oauthlib import OAuth2Session
 
 
 class OAuthbasedUserManager(FilebasedUserManager):
-    def __init__(self, components, settings):
-        logging.getLogger("octoprint.plugins." + __name__).info("#######2222######")
-        self._components = components
-        self._settings = settings
-        self.oauth2 = self._settings.get(["plugins", "oauth2"])
-        FilebasedUserManager.__init__(self)
+	def __init__(self, components, settings):
+		logging.getLogger("octoprint.plugins." + __name__).info("#######2222######")
+		self._components = components
+		self._settings = settings
+		# Get data from config file
+		self.oauth2 = self._settings.get(["plugins", "oauth2"])
+		self.PATH_FOR_TOKEN = self.oauth2["token_path"]
+		self.PATH_USER_INFO = self.oauth2["user_info_path"]
+		self.USERNAME_KEY = self.oauth2["username_key"]
 
-    def logout_user(self, user):
-        logging.getLogger("octoprint.plugins." + __name__).info("OAuth Logging out")
-        UserManager.logout_user(self, user)
+		# These three will be initialized later.
+		# CLIENT_ID and CLIENT_SECRET need REDIRECT_URI for proper initialization
+		self.CLIENT_ID = None
+		self.CLIENT_SECRET = None
+		self.REDIRECT_URI = None
 
-    def get_token(self, code):
+		# Init FilebasedUserManager, other methods are needed for OctoPrint
+		FilebasedUserManager.__init__(self)
 
-        post_data = {"grant_type": "authorization_code",
-                     "code": code,
-                     "redirect_uri": self.REDIRECT_URI,
-                     "client_id": self.CLIENT_ID,
-                     "client_secret": self.CLIENT_SECRET
-                     }  # json required
+	def logout_user(self, user):
+		logging.getLogger("octoprint.plugins." + __name__).info("OAuth Logging out")
+		UserManager.logout_user(self, user)
 
-        response = requests.post(self.PATH_FOR_TOKEN,
-                                 data=post_data,
-                                 headers=self.TOKEN_HEADERS)
+	def get_token(self, oauth, code):
 
-        try:
-            token_json = response.json()
-        except ValueError:
-            logging.getLogger("octoprint.plugins." + __name__).error("JSON required, perhaps you forgot to specific it with token headers")
-            return None
+		# fetching token using requests_oauthlib library
+		token_json = oauth.fetch_token(self.PATH_FOR_TOKEN,
+									   authorization_response="authorization_code",
+									   code=code,
+									   client_id=self.CLIENT_ID,
+									   client_secret=self.CLIENT_SECRET,
+									   headers=self.TOKEN_HEADERS)
 
-        try:
-            # token is OK
-            access_token = token_json["access_token"]
-            return access_token
-        except KeyError:
-            try:
-                error = token_json["error"]
-                logging.getLogger("octoprint.plugins." + __name__).error("Error of access token: " + error)
-            except:
-                logging.getLogger("octoprint.plugins." + __name__).error("Error of access token, error message not found")
+		try:
+			# token is OK
+			access_token = token_json["access_token"]
+			return access_token
+		except KeyError:
+			try:
+				error = token_json["error"]
+				logging.getLogger("octoprint.plugins." + __name__).error("Error of access token: " + error)
+			except:
+				logging.getLogger("octoprint.plugins." + __name__).error("Error of access token, error message not found")
 
-        return None
+		return None
 
-    def get_username(self, access_token):
+	def get_username(self, oauth, access_token):
 
-        params = {
-            "access_token": access_token
-        }
+		# GET user data from resource server
+		response = oauth.get(self.PATH_USER_INFO)
+		data = response.json()
 
-        response = requests.get(self.PATH_USER_INFO,
-                                params=params)
+		# Try if data contains USERNAME_KEY from config file
+		try:
+			login = data[self.USERNAME_KEY]
+			return login
+		except KeyError:
+			logging.getLogger("octoprint.plugins." + __name__).error("User data does not contain username key:" + self.USERNAME_KEY)
 
-        data = response.json()
+		return None
 
-        try:
-            login = data[self.USERNAME_KEY]
-            return login
-        except KeyError:
-            pass
+	def login_user(self, user):
 
-        return None
+		self._cleanup_sessions()
 
-    def login_user(self, user):
+		if user is None:
+			return
 
-        self._cleanup_sessions()
+		if isinstance(user, LocalProxy):
+			user = user._get_current_object()
+			return user
 
-        if user is None:
-            return
+		if not isinstance(user, User):
+			return None
 
-        # LocalProxy.
+		if not isinstance(user, SessionUser):
+			code = user.get_id()
+			try:
+				self.TOKEN_HEADERS = self.oauth2["token_headers"]
+			except KeyError:
+				self.TOKEN_HEADERS = None
+			oauth = OAuth2Session(self.CLIENT_ID,
+								  redirect_uri=self.REDIRECT_URI)
+			access_token = self.get_token(oauth, code)
 
-        if isinstance(user, LocalProxy):
-            user = user._get_current_object()
-            return user
+			if access_token is None:
+				return None
 
-        if not isinstance(user, User):
-            return None
+			username = self.get_username(oauth, access_token)
+			user = FilebasedUserManager.findUser(self, username)
+			if username is None:
+				logging.getLogger("octoprint.plugins." + __name__).error("Username none")
+				return None
 
+			if user is None:
+				self.addUser(username, "", True, ["user"])
+				user = self.findUser(username)
 
-        if not isinstance(user, SessionUser):
-            code = user.get_id()
+		if not isinstance(user, SessionUser):
+			user = SessionUser(user)
 
-            self.CLIENT_ID = self.oauth2[self.REDIRECT_URI]["client_id"]
-            self.CLIENT_SECRET = self.oauth2[self.REDIRECT_URI]["client_secret"]
-            self.PATH_FOR_TOKEN = self.oauth2["token_path"]
-            self.PATH_USER_INFO = self.oauth2["user_info_path"]
-            self.USERNAME_KEY = self.oauth2["username_key"]
-            try:
-                self.TOKEN_HEADERS = self.oauth2["token_headers"]
-            except KeyError:
-                self.TOKEN_HEADERS = None
-            access_token = self.get_token(code)
+		self._session_users_by_session[user.session] = user
 
-            if access_token is None:
-                return None
+		user_id = user.get_id()
+		if not user_id in self._sessionids_by_userid:
+			self._sessionids_by_userid[user_id] = set()
 
-            username = self.get_username(access_token)
-            user = FilebasedUserManager.findUser(self,username)
-            if username is None:
-                logging.getLogger("octoprint.plugins." + __name__).error("Username none")
-                return None
+		self._sessionids_by_userid[user_id].add(user.session)
+		return user
 
-            if user is None:
-                self.addUser(username, "", True, ["user"])
-                user = self.findUser(username)
+	def addUser(self, username, password, active=False, roles=None, apikey=None, overwrite=False):
+		if not roles:
+			roles = ["user"]
 
+		if username in self._users.keys() and not overwrite:
+			raise UserAlreadyExists(username)
 
-        if not isinstance(user, SessionUser):
-            user = SessionUser(user)
+		# Add user with password and not creating passwordhash.
+		self._users[username] = User(username, password, active, roles, apikey=apikey)
+		self._dirty = True
+		self._save()
 
-        self._session_users_by_session[user.session] = user
+	def checkPassword(self, username, password):
+		logging.getLogger("octoprint.plugins." + __name__).info("Logging in via OAuth 2.0")
+		self.REDIRECT_URI = password
+		return True
 
-        userid = user.get_id()
-        if not userid in self._sessionids_by_userid:
-            self._sessionids_by_userid[userid] = set()
+	def findUser(self, userid=None, apikey=None, session=None):
 
-        self._sessionids_by_userid[userid].add(user.session)
-        return user
+		user = FilebasedUserManager.findUser(self, userid, apikey, session)
+		if user is not None:
+			return user
 
-    def addUser(self, username, password, active=False, roles=None, apikey=None, overwrite=False):
-        if not roles:
-            roles = ["user"]
-
-        if username in self._users.keys() and not overwrite:
-            raise UserAlreadyExists(username)
-
-        self._users[username] = User(username, password, active, roles, apikey=apikey)
-        self._dirty = True
-        self._save()
-
-    def checkPassword(self, username, password):
-        logging.getLogger("octoprint.plugins." + __name__).info("Logging in via OAuth 2.0")
-        self.REDIRECT_URI = password
-        return True
-
-    def findUser(self, userid=None, apikey=None, session=None):
-
-        user = FilebasedUserManager.findUser(self, userid, apikey, session)
-        if user is not None:
-            return user
-
-        # making temporary user because of implementation of api
-        # and we need to pass our code from OAuth to login_user
-        # api login could be found in server/api/__init__.py
-        user = User(userid, "", 1, ["user"])
-        return user
+		# making temporary user because of implementation of api
+		# and we need to pass our code from OAuth to login_user
+		# api login could be found in server/api/__init__.py
+		user = User(userid, "", 1, ["user"])
+		return user
